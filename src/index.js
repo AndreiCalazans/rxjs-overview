894a70521c5c39b52e8ffafe0bb1259e5d0d7d46
AndreiCalazans <andreixoc@hotmail.com>
2019-02-02 10:50:33 -0200

    Introduction: Purity

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import { fromEvent } from 'rxjs';
+import { scan } from 'rxjs/operators';
 
 const button = document.querySelector('button');
+let count = 0;
+
 // Without from event
-button.addEventListener('click', () => console.log('Clicked'));
+button.addEventListener('click', () => console.log(`Clicked: ${++count} times`));
 
 // with from event
 fromEvent(button, 'click')
-  .subscribe(() => console.log("stream clicked"));
+  .pipe(scan(count => count + 1, 0))
+  .subscribe(count => console.log(`Stream clicked: ${count} times`));
 
