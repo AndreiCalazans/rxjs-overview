db87eb9747e28abbce045886004cf5f54ddd0fc4
AndreiCalazans <andreixoc@hotmail.com>
2019-02-02 10:55:31 -0200

    Introduction: Flows

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import { fromEvent } from 'rxjs';
-import { scan } from 'rxjs/operators';
+import { scan, throttleTime } from 'rxjs/operators';
 
 const button = document.querySelector('button');
+
 let count = 0;
+let rate = 1000;
+var lastClick = Date.now() - rate;
+button.addEventListener('click', () => {
+  if (Date.now() - lastClick >= rate) {
+    console.log(`Clicked ${++count} times`);
+    lastClick = Date.now();
+  }
+})
 
-// Without from event
-button.addEventListener('click', () => console.log(`Clicked: ${++count} times`));
 
-// with from event
+// With RXJS
 fromEvent(button, 'click')
-  .pipe(scan(count => count + 1, 0))
-  .subscribe(count => console.log(`Stream clicked: ${count} times`));
-
+  .pipe(
+    throttleTime(1000),
+    scan(count => count + 1, 0)
+  )
+  .subscribe(count => console.log(`Stream cliked: ${count} times`))
